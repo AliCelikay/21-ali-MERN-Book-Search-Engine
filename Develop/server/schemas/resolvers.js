4ed4bd340ff7ae2f589a0a8fbf62ac87a3b5835d
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-11-05 12:03:17 -0500

    query.js and mutation.js complete

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 // for authentication functions in resolvers object
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
